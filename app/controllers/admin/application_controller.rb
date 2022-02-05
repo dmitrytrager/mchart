9bc7f1df2f499a604227293380921928e5b6e9c2
dmitrytrager <dmitry.trager@revealbot.com>
2022-02-06 01:02:03 +0400

    Add authentication logic, fix views

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -8,12 +8,13 @@
 # you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
-    # before_action :authenticate_user!
+    before_action :authenticate_user!
     before_action :authenticate_admin
 
     def authenticate_admin
-      # TODO: Add authentication logic here.
-      # current_user.admin?
+      return if current_user.admin?
+
+      redirect_to root_path
     end
 
     # Override this value to specify the number of elements to display at a time
