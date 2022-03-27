00384e085cd09878179fd7aade8505e0f8ff4a51
dmitrytrager <dmitry.trager@revealbot.com>
2022-03-27 12:01:47 +0400

    Fix karma reset points spec

--- spec/services/karma_reset_points_spec.rb
+++ spec/services/karma_reset_points_spec.rb
@@ -3,17 +3,20 @@
 require "rails_helper"
 
 RSpec.describe KarmaResetPoints do
-  let!(:user)       { create(:user) }
-  let!(:other_user) { create(:user) }
+  let!(:user)        { create(:user) }
+  let!(:other_user1) { create(:user) }
+  let!(:other_user2) { create(:user) }
 
   before do
-    user.update(karma: 4)
+    create :karma_vote, voter_id: other_user1.id, votee_id: user.id
+    create :karma_vote, voter_id: other_user2.id, votee_id: user.id
   end
 
   it "updates evety user's karma points with karma value" do
     subject.call
 
-    expect(user.reload.karma_points).to eq(4)
-    expect(other_user.reload.karma_points).to be_zero
+    expect(user.reload.karma_points).to eq(2)
+    expect(other_user1.reload.karma_points).to be_zero
+    expect(other_user2.reload.karma_points).to be_zero
   end
 end
