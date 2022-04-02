fe0168c3888211383c18952637934cba9a2fb919
dmitrytrager <dmitry.trager@revealbot.com>
2022-04-02 23:28:26 +0400

    Fix like create spec

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,6 +4,6 @@ class LikesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    Likes::Vote.new(current_user, Charts.find(params[:chart_id])).call
+    Likes::Vote.new(current_user, Chart.find(params[:chart_id])).call
   end
 end
--- spec/requests/likes/create_spec.rb
+++ spec/requests/likes/create_spec.rb
@@ -8,9 +8,24 @@ describe "POST /charts/:chart_id/likes", type: :request do
   let(:chart_id) { chart.id }
   let(:url)      { "/charts/#{chart_id}/likes" }
 
-  it "creates likes" do
-    post(url, params: {}, headers: {})
+  before do
+    user.update(confirmed_at: Time.zone.now)
+    sign_in(user)
+  end
 
-    expect(chart.reload.likes.size).to eq(1)
+  it "does not create likes" do
+    post(url)
+
+    expect(chart.reload.likes.size).to eq(0)
+  end
+
+  context "when having some karma_points" do
+    before { user.update(karma_points: 2) }
+
+    it "creates likes" do
+      post(url)
+
+      expect(chart.reload.likes.size).to eq(1)
+    end
   end
 end
